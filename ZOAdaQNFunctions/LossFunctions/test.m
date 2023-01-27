7d9d1d919fefc186ff1433d910c33b1eb6515617
raghubollapragada <85360562+raghubollapragada@users.noreply.github.com>
2023-01-27 10:51:36 -0600

    Update test.m
    Updated a comment regarding this function

--- ZOAdaQNFunctions/LossFunctions/test.m
+++ ZOAdaQNFunctions/LossFunctions/test.m
@@ -1,6 +1,7 @@
 function [f, g] = test(inputvals, w)
     % TEST Summary of this function goes here
-    %   Detailed explanation goes here
+    % This function is a place holder for implementing objective functions 
+    % corrsponding to test datasets for solving optimization problems arising in machine learning
     f = 0;
     g = 0;
 end
