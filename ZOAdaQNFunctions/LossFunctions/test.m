6d55bc3554aedd7d17be71e9959d267bee080028
Stefan M Wild <wild@lbl.gov>
2023-01-28 08:29:23 -0600

    Update test.m

--- ZOAdaQNFunctions/LossFunctions/test.m
+++ ZOAdaQNFunctions/LossFunctions/test.m
@@ -1,6 +1,6 @@
 function [f, g] = test(inputvals, w)
     % TEST Summary of this function goes here
-    % This function is a place holder for implementing objective functions 
+    % This function is a place holder for implementing objective functions
     % corrsponding to test datasets for solving optimization problems arising in machine learning
     f = 0;
     g = 0;
